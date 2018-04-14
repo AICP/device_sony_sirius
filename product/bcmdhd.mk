67b7a8f0cfe55f3970c8153c7cec7aa218f8a624
Hafyzy <hafizi_pg04@yahoo.com>
2018-04-15 00:26:04 +0800

    revert " Move device specific system configs into /vendor/etc "
    Change-Id: Id723c47bd403cd212d728edcf12eec4d2416a5bd

--- product/bcmdhd.mk
+++ product/bcmdhd.mk
@@ -1,3 +1,3 @@
 # Wifi
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/bcmdhd/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/firmware/bcmdhd.cal
+   $(LOCAL_PATH)/bcmdhd/bcmdhd.cal:system/etc/wifi/firmware/bcmdhd.cal
